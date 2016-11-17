cae0d098260d2bf7e7106c86f78d267b9bbec885
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-17 15:30:21 +0530

    Adding script on Thu Nov 17 15:30:21 IST 2016 :tornado_cloud:,

--- js/app.js
+++ js/app.js
@@ -4,17 +4,14 @@ $(document).ready(function() {
 
 	$.getJSON("https://raw.githubusercontent.com/deadcoder0904/a2k-quotes/master/js/quotes.json", function( data ) {  
     for (var i = 0; i < data.length; i++) {
-			  arr.push("<div class='pa4 tc'>");
+			  arr.push("<div class='pa4'>");
 				arr.push("<blockquote class='athelas ml0 mt0 pl4 black-90 bl bw2 b--blue'>");
-				arr.push("<p class='f5 f4-m f3-l lh-copy measure mt0'>");  
-				arr.push(data[i].quote);
-				arr.push("</p>");
-				arr.push("<cite class='f6 ttu tracked fs-normal'>");
-			  arr.push(data[i].author);
-			  arr.push("</cite>");
+				arr.push("<p class='f5 f4-m f3-l lh-copy measure mt0'>" + data[i].quote + "</p>");
+				arr.push("<cite class='f6 ttu tracked fs-normal'>" + data[i].author + "</cite>");
 			  arr.push("</blockquote>");
 				arr.push("</div>");
 		}
+		console.log(arr);
 		quotes.append(arr.join(""));
 	});
 
