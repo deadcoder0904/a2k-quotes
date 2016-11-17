5ff873a6500dc76760cb98fdd0e598d84d906acb
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-17 15:11:45 +0530

    Adding script on Thu Nov 17 15:11:45 IST 2016 :radioactive_sign:,

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
 
 	$.getJSON("https://raw.githubusercontent.com/deadcoder0904/a2k-quotes/master/js/quotes.json", function( data ) {  
     for (var i = 0; i < data.length; i++) {
-			  console.log(data[i].quote + " -> " + data[i].author);
-			  arr.push("<div class='pa4'>");
+			  arr.push("<div class='pa4 tc'>");
 				arr.push("<blockquote class='athelas ml0 mt0 pl4 black-90 bl bw2 b--blue'>");
 				arr.push("<p class='f5 f4-m f3-l lh-copy measure mt0'>");  
 				arr.push(data[i].quote);
@@ -18,6 +17,5 @@ $(document).ready(function() {
 		}
 		quotes.append(arr.join(""));
 	});
-	console.log(quotes);
-	console.log(arr);
+
 });
